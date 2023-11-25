0c2c287fc9ad6e54004aa8d5def96902a0eba282
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-25 16:32:40 +0300

    ok

---
+++ reading-json/bin/reading_json.dart
@@ -0,0 +1,5 @@
+import 'package:reading_json/reading_json.dart' as reading_json;
+
+void main(List<String> arguments) {
+  print('Hello world: ${reading_json.calculate()}!');
+}
---
+++ reading-json/test/reading_json_test.dart
@@ -0,0 +1,8 @@
+import 'package:reading_json/reading_json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
