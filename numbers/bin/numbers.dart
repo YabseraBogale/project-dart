6fd02a3f276deea26ccab2996bf040c4fa81901a
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-26 10:14:40 +0300

    ok

--- numbers/bin/numbers.dart
+++ numbers/bin/numbers.dart
@@ -5,6 +5,11 @@ void main(List<String> arguments) {
   int num1 = 10;
   int num2 = 5;
   print("max number from $num1 and $num2 is ${max(num1, num2)}");
-  var zerodegree = sin(0);
-  print("sin of 0 is $zerodegree");
+  var state = cos((60 * pi) / 180) == sin((30 * pi) / 180);
+  var six = cos((60 * pi) / 180);
+  var three = sin((30 * pi) / 180);
+  print("sin of 30 $three");
+  print("sin of 60 $six");
+  print(
+      "Direct comparsion doesn't work sin of 30 is equal with coss 60 but return $state");
 }
