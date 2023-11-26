2b3616034222ca638e2ef6d28fdcd0df144d74ad
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-26 10:05:55 +0300

    ok

--- numbers/bin/numbers.dart
+++ numbers/bin/numbers.dart
@@ -5,4 +5,6 @@ void main(List<String> arguments) {
   int num1 = 10;
   int num2 = 5;
   print("max number from $num1 and $num2 is ${max(num1, num2)}");
+  var zerodegree = sin(0);
+  print("sin of 0 is $zerodegree");
 }
