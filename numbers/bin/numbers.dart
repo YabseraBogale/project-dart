4bfc80d88868abce0f906028fdedd8d4e2c69126
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-26 10:03:31 +0300

    ok

--- numbers/bin/numbers.dart
+++ numbers/bin/numbers.dart
@@ -4,5 +4,5 @@ import 'dart:math';
 void main(List<String> arguments) {
   int num1 = 10;
   int num2 = 5;
-  print("max number from $num1 and $num2 ${max(num1, num2)}");
+  print("max number from $num1 and $num2 is ${max(num1, num2)}");
 }
