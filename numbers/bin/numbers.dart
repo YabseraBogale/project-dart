092b892a4458789a29a95f75285414a43c1cb9c2
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-26 10:26:50 +0300

    ok

--- numbers/bin/numbers.dart
+++ numbers/bin/numbers.dart
@@ -1,9 +1,11 @@
+import 'dart:html';
 import 'package:numbers/numbers.dart' as numbers;
 import 'dart:math';
 
 void main(List<String> arguments) {
   int num1 = 10;
   int num2 = 5;
+  print("number $num1 is Even ${num1.isEven}");
   print("max number from $num1 and $num2 is ${max(num1, num2)}");
   var state = cos((60 * pi) / 180) == sin((30 * pi) / 180);
   var six = cos((60 * pi) / 180);
