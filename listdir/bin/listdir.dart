47727da199814cbfae5597a17fec0749dbf7f73f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-28 15:40:12 +0300

    ok

--- listdir/bin/listdir.dart
+++ listdir/bin/listdir.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 void main() {
   var start = Directory('.');
-  //num count = 0;
+  num count = 0;
   for (var i in start.listSync(recursive: true, followLinks: true)) {
-    print(i.runtimeType.toString() == "_File");
+    if (i.runtimeType.toString() == "_File") {
+      count += 1;
+    }
   }
-  //print(count);
+  print(count);
 }
