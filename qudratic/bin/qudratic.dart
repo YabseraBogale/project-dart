b929c81d6740ee17d48beedfda7f069cd43d9bae
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-05-02 19:26:01 +0300

    ok

--- qudratic/bin/qudratic.dart
+++ qudratic/bin/qudratic.dart
@@ -3,5 +3,10 @@ import 'dart:io';
 void main() {
   stdout.write("Enter number a=");
   num? a = num.parse(stdin.readLineSync()!);
-  print(a);
+  stdout.write("Enter number b=");
+  num? b = num.parse(stdin.readLineSync()!);
+  stdout.write("Enter number c=");
+  num? c = num.parse(stdin.readLineSync()!);
+
+  print('$a, $b, $c');
 }
