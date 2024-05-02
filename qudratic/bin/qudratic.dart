93c23043a37a343b8fdc13294bfaca82b15ff639
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-05-02 19:11:49 +0300

    ok

--- qudratic/bin/qudratic.dart
+++ qudratic/bin/qudratic.dart
@@ -1,14 +1,6 @@
 import 'dart:io';
 
 void main() {
-  final a;
-  final b;
-  final c;
-  print("Enter the coffeinents of \na=");
-  a = stdin.readByteSync().toInt();
-
-  b = stdin.readByteSync().toInt();
-
-  c = stdin.readByteSync().toInt();
-  print('$a, $b, $c');
+  int? a = stdin.readByteSync();
+  print(a);
 }
