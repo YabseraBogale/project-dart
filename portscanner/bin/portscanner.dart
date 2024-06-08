b1111126db3f47aed6d53ef51c6ba5e09910c705
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-08 20:26:07 +0300

    ok

--- portscanner/bin/portscanner.dart
+++ portscanner/bin/portscanner.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  var res =
-      await Socket.connect("https://dart.dev/tutorials/server/fetch-data", 80);
-  print(res);
+  var clinet = HttpClient();
+  var res = await clinet.getUrl(Uri.https("google.com"));
+  print(res.headers.contentLength);
 }
