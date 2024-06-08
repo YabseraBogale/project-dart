362406613cfc93059efeb55446851332f6dd9f4e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-08 17:50:20 +0300

    ok

--- scrape/bin/scrape.dart
+++ scrape/bin/scrape.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  var url = Uri.https("clouddevs.com", "/dart/web-scraping/");
-  print(url);
-  var client = HttpClient();
-  var res = client.get("clouddevs.com", 80, "/dart/web-scraping/");
-  print(res);
+void main(List<String> arguments) async {
+  final go = await http
+      .read(Uri.parse("https://dart.dev/tutorials/server/fetch-data"));
+  print(go);
 }
