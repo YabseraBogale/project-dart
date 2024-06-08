c8dd9ad3e150f5f73de4dab7539bb91d7cb92c26
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-08 17:37:21 +0300

    ok

--- scrape/bin/scrape.dart
+++ scrape/bin/scrape.dart
@@ -3,5 +3,7 @@ import 'dart:io';
 void main(List<String> arguments) {
   var url = Uri.https("clouddevs.com", "/dart/web-scraping/");
   print(url);
-  var req = HttpRequest();
+  var client = HttpClient();
+  var res = client.get("clouddevs.com", 80, "/dart/web-scraping/");
+  print(res);
 }
